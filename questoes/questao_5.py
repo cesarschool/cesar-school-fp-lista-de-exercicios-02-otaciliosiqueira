54b9f0f76f104bc2c96611b157b9e49aafa353df
Otacílio Siqueira <34538777+otaciliosiqueira@users.noreply.github.com>
2018-04-10 00:35:23 -0300

    RESOLVIDA QUESTAO 5

--- questoes/questao_5.py
+++ questoes/questao_5.py
@@ -11,9 +11,18 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 5")
-
-
     
+    a = float(input('Digite o valor do primeiro lado:  '))
+    b = float(input('Digite o valor do segundo lado:  '))
+    c = float(input('Digite o valor do terceiro lado:  '))
+
+    print('\nDe acordo com os valores digitados temos um triângulo:\n')
+    if a == b and b == c:
+        print('EQUILATERO')
+    elif a == b or a == c or b == c:
+        print('ISOSCELES')
+    else:
+        print('ESCALENO')
+        
 if __name__ == '__main__':
     main()
