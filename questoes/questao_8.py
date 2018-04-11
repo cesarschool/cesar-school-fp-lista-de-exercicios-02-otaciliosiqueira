1856495586d37755a9fc7c4f4eb4c33ae7976975
Otacílio Siqueira <34538777+otaciliosiqueira@users.noreply.github.com>
2018-04-10 23:57:51 -0300

    RESOLVIDA QUESTAO 8

--- questoes/questao_8.py
+++ questoes/questao_8.py
@@ -25,9 +25,7 @@ def main():
 
 
     anoBissexto = ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0
-    cond = False
-
-
+   
     if (mes==1 or mes==3 or mes==5 or mes==7 or mes==8 or mes==10 or mes==12):
         if dia==31:
             if mes==12:
